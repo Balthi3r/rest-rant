e8f3b8162d3ae49b398bd9c3e15177c0bd24fef9
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-07-27 18:52:05 -0500

    got test post to work in console

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,5 @@
 const router = require('express').Router()
-
-router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
-  
+const places = require("../models/places")
 
 // GET /places
 router.get('/', (req, res) => {
@@ -24,6 +20,17 @@ router.get('/', (req, res) => {
       res.render('places/index', { places })
 
   })
+
+  router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+  })
+  
+  
   
 
 module.exports = router

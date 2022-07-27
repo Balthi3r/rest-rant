def5cc920a2851d7d1e0379eca83715694da5c9c
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-07-27 18:58:17 -0500

    completed part 5 with post

--- controllers/places.js
+++ controllers/places.js
@@ -3,32 +3,29 @@ const places = require("../models/places")
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'The Taco Zone',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Mexican',
-        pic: '/images/tacos.jpg'
-      }, {
-        name: 'Brunch and Code Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/waffles.jpg'
-      }]
-      
-      res.render('places/index', { places })
-
+    res.render('places/index', { places })
   })
 
-  router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
 
-  router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-  })
+router.post('/', (req, res) => {
+  // console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
   
   
   
--- models/places.js
+++ models/places.js
@@ -1,13 +1,14 @@
 module.exports = [{
-	name: 'H-Thai-ML',
-	city: 'Seattle',
-	state: 'WA',
-	cuisines: 'Thai, Pan-Asian',
-	pic: '/images/h-thai-ml-tables.jpg'
-    }, {
-	name: 'Coding Cat Cafe',
-	city: 'Phoenix',
-	state: 'AZ',
-	cuisines: 'Coffee, Bakery',
-	pic: '/images/coffee-cat.png'
-    }]
+    name: 'The Taco Zone',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Mexican',
+    pic: '/images/tacos.jpg'
+    }, 
+    {
+    name: 'Brunch and Code Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/waffles.jpg'
+  }]

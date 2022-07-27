34c90637e8c03ed3dea1cc5f9b584a4a85a54d86
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-07-26 22:46:36 -0500

    completed parts 3 and 4 - starting pt5

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,29 @@
-const router = require ('express').Router()
+const router = require('express').Router()
 
-router.get ('/', (req, res) => {
-    res.send ('GET /places')
-})
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
+
+// GET /places
+router.get('/', (req, res) => {
+    let places = [{
+        name: 'The Taco Zone',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Mexican',
+        pic: '/images/tacos.jpg'
+      }, {
+        name: 'Brunch and Code Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/waffles.jpg'
+      }]
+      
+      res.render('places/index', { places })
+
+  })
+  
+
 module.exports = router
--- index.js
+++ index.js
@@ -2,13 +2,17 @@ require ('dotenv').config()
 const express = require ('express')
 const app = express ()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require ('express-react-views').createEngine())
+app.use(express.static('public'))
+
 app.use ('/places', require('./controllers/places'))
 app.get ('/', (req, res) => {
-    res.send ('hello world')
+    res.render ('home')
 })
 
 //Wildcard Route kindly tell them page does not exist
 app.get ('*', (req, res) => {
-    res.status(404).send ('<h1>404 Page</>')
+    res.render ('error404')
 })
 app.listen (process.env.PORT)

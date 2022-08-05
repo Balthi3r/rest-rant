2f677a8269937c3e386ab277b4812d0c0b92aa4e
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-08-05 17:04:31 -0500

    completed pt 9

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,24 +5,47 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
-        <div>
-          <h3>Rating</h3>
-          <p>Not Rated</p>
+        <div className="row">
+          <div className="col-sm-6">
+            <img
+              className="col-sm-6"
+              src={data.place.pic}
+              alt={data.place.name}
+            />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <br />
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+            <br />
+            <a
+              href={`/places/${data.place.id}/edit`}
+              className="btn btn-warning"
+            >
+              Edit
+            </a>
+            {` `}
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
         </div>
-        <div>
-          <h3>Description</h3>
-          <p> </p>
-        </div>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
+        <hr />
+            <h2>Comments</h2>
+            <div className="row">
+              
+            </div>
       </main>
     </Def>
   );
